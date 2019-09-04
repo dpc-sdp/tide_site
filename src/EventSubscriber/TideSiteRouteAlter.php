2f3c747f494de16ad265e832b896457452c66c6e
Vincent Gao <vincent.gao@dpc.vic.gov.au>
2019-09-04 13:37:48 +1000

    SDPA-3154 Fixed performance issue

--- src/EventSubscriber/TideSiteRouteAlter.php
+++ src/EventSubscriber/TideSiteRouteAlter.php
@@ -18,15 +18,11 @@ class TideSiteRouteAlter extends RouteSubscriberBase {
    * {@inheritDoc}.
    */
   protected function alterRoutes(RouteCollection $collection) {
-    // Comment it out temporarily for debugging why does
-    // the view.summary_contents.page load pretty slow.
-    /*
     $route = $collection->get('system.admin_content');
     $summary_contents_route = $collection->get('view.summary_contents.page');
     if ($route && $summary_contents_route) {
-    $collection->add('system.admin_content', clone $summary_contents_route);
+      $collection->add('system.admin_content', clone $summary_contents_route);
     }
-     */
   }
 
 }
