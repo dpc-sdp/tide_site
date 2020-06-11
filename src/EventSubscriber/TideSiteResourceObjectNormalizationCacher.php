bb894116bd429041b4e89dad06a5163ff7b4beaa
vincent-gao <vincent.gao@dpc.vic.gov.au>
2020-06-11 11:19:40 +1000

    [SDPA-4225] updates tide_site to be compatible with Drupal 8.8.5 (#55)
    * SDPA-4225 update tide_site to accomplish drupal8.8
    
    * SDPA-4225 refactores path alias and linkit code
    
    * Update test scenario
    
    * updates
    
    1. hook_tide_link_enhancer_transform_alter
    2. logics inside tide_site_path_alias_insert
    3. alter path_alias.manager
    
    * override ResourceObjectNormalizationCacher
    
    * fixes lint issue
    
    * fixes issues
    
    1. cannot find Drupal\path_alias\AliasRepository
    
    * adds tide_site_path_alias_update and its helper
    
    * updates
    
    1. use $this->entityTypeManager not  \Drupal::entityTypeManager() in a service class
    
    * delete not necessary function
    
    delete public function loadAll() from AliasStorageHelper class
    
    * rename getNodeFromPath to getNodeFromPathEntity
    
    * update fields.media.feature
    
    video bundle has been removed from tide_media. so delete it.
    
    * updates
    
    1. add a submit handler for deleting all path alias from path alias entity form.
    2. updates deleteSiteAliases() and updateSiteAliases() function
    
    * Update tide_site.module file
    
    * update dependencies
    
    tide_core , tide_media, tide_api, dev-tools
    
    * Fixes building error.
    
    Drupal\Component\Plugin\Exception\PluginNotFoundException: The [error] "path_alias" entity type does not exist
    
    * Fixes PHP fatal error issue in client site
    
    * updates
    
    1. need a function to delete all aliases
    2. check if the alias has a site prefix
    
    * update dependencies
    
    1. tide_core 2.0.0
    2. tide_media 2.0.0
    3. tide_api 2.0.0
    
    * uses master branch of dev_tools

---
+++ src/EventSubscriber/TideSiteResourceObjectNormalizationCacher.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\tide_site\EventSubscriber;
+
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\jsonapi\EventSubscriber\ResourceObjectNormalizationCacher;
+use Drupal\jsonapi\JsonApiResource\ResourceObject;
+
+/**
+ * Caches entity normalizations after the response has been sent.
+ */
+class TideSiteResourceObjectNormalizationCacher extends ResourceObjectNormalizationCacher {
+
+  /**
+   * Writes a normalization to cache.
+   *
+   * @see \Drupal\jsonapi\EventSubscriber\ResourceObjectNormalizationCacher
+   */
+  protected function set(ResourceObject $object, array $normalization_parts) {
+    $base = static::generateLookupRenderArray($object);
+    $data_as_render_array = $base + [
+        // The data we actually care about.
+      '#data' => $normalization_parts,
+        // Tell RenderCache to cache the #data property: the data we actually
+        // care about.
+      '#cache_properties' => ['#data'],
+        // These exist only to fulfill the requirements of the RenderCache,
+        // which is designed to work with render arrays only. We don't care
+        // about these.
+      '#markup' => '',
+      '#attached' => '',
+    ];
+
+    // Merge the entity's cacheability metadata with that of the normalization
+    // parts, so that RenderCache can take care of cache redirects for us.
+    CacheableMetadata::createFromObject($object)
+      // Adds url.query_args:site to the normalization.
+      ->addCacheContexts(['url.query_args:site'])
+      ->merge(static::mergeCacheableDependencies($normalization_parts[static::RESOURCE_CACHE_SUBSET_BASE]))
+      ->merge(static::mergeCacheableDependencies($normalization_parts[static::RESOURCE_CACHE_SUBSET_FIELDS]))
+      ->applyTo($data_as_render_array);
+
+    $this->renderCache->set($data_as_render_array, $base);
+  }
+
+}
