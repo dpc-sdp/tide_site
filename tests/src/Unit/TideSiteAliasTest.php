5e19db4313b6855f1e3aac3186960319e015a711
Vincent Gao <vincent.gao@dpc.vic.gov.au>
2021-05-11 23:16:18 +1000

    updates alias unit test.

--- tests/src/Unit/TideSiteAliasTest.php
+++ tests/src/Unit/TideSiteAliasTest.php
@@ -1,34 +1,40 @@
 <?php
 
-
 namespace Drupal\Tests\tide_site\Unit;
 
-use Drupal\Core\Entity\EntityTypeManager;
-use Drupal\path_alias\Entity\PathAlias;
 use Drupal\Tests\UnitTestCase;
-use Drupal\tide_site\AliasManager;
 use Drupal\tide_site\AliasStorageHelper;
 use Drupal\tide_site\TideSiteHelper;
 
 class TideSiteAliasTest extends UnitTestCase {
 
   /**
-   * Path alias manager.
-   * @var \Drupal\tide_site\AliasManager|\PHPUnit\Framework\MockObject\MockObject
-   */
-  protected $aliasManager;
-
-  /**
    * Entity type manager.
+   *
    * @var \Drupal\Core\Entity\EntityTypeManager|\PHPUnit\Framework\MockObject\MockObject
    */
   protected $entityTypeManager;
 
   /**
    * Path alias helper function.
+   *
    * @var \Drupal\tide_site\AliasStorageHelper|\PHPUnit\Framework\MockObject\MockObject
    */
   protected $aliasHelper;
+
+  /**
+   * Tide site helper.
+   *
+   * @var \Drupal\tide_site\TideSiteHelper|\PHPUnit\Framework\MockObject\MockObject
+   */
+  protected $tideSiteHelper;
+
+  /**
+   * Entity storage.
+   *
+   * @var \PHPUnit\Framework\MockObject\MockObject|Drupal\Core\Entity\ContentEntityNullStorage
+   */
+  protected $entityStorage;
 
 
   /**
@@ -36,39 +42,21 @@ class TideSiteAliasTest extends UnitTestCase {
    */
   function setUp() {
     parent::setUp();
-    // Mock out services and entities.
-    $this->aliasManager = $this->createMock(AliasManager::class);
-    $this->entityTypeManager = $this->createMock(EntityTypeManager::class);
-    $methods = get_class_methods('Drupal\tide_site\AliasStorageHelper');
-    unset($methods[array_search('getPathAliasWithoutSitePrefix', $methods)]);
-    $this->aliasHelper = $this->getMockBuilder('Drupal\tide_site\AliasStorageHelper')
+    $methods = get_class_methods('Drupal\Core\Entity\ContentEntityNullStorage');
+    $this->entityStorage = $this->getMockBuilder('Drupal\Core\Entity\ContentEntityNullStorage')
       ->disableOriginalConstructor()
       ->setMethods($methods)
       ->getMock();
-    $alias_matrix = [
-      ['/node/1', NULL, '/site-1/hello'],
-      ['/node/2', NULL, '/site-2/hello'],
-      ['/node/3', NULL, '/site-3/hello'],
-      ['/node/4', NULL, '/site-4/hello'],
-      ['/node/5', NULL, '/site-5/hello'],
-      ['/node/6', NULL, '/site-6/hello'],
-    ];
-    $this->aliasManager->expects($this->any())
-      ->method('getAliasByPath')
-      ->will($this->returnValueMap($alias_matrix));
-  }
-
-  function testUniquenessOfAlias() {
-    $this->assertEquals('/site-1/hello', $this->aliasManager->getAliasByPath('/node/1'));
-    $this->assertEquals('/site-2/hello', $this->aliasManager->getAliasByPath('/node/2'));
-  }
-
-  /**
-   * @dataProvider providerAliasWithoutSite
-   */
-  function testgetPathAliasWithoutSitePrefix($alias_with_site, $expection) {
-    $without_siteid = $this->aliasHelper->getPathAliasWithoutSitePrefix($alias_with_site);
-    $this->assertEquals($expection, $without_siteid);
+    $methods = get_class_methods('Drupal\Core\Entity\EntityTypeManager');
+    $this->entityTypeManager = $this->getMockBuilder('Drupal\Core\Entity\EntityTypeManager')
+      ->disableOriginalConstructor()
+      ->setMethods($methods)
+      ->getMock();
+    $this->entityTypeManager->expects($this->any())
+      ->method('getStorage')
+      ->willReturn($this->entityStorage);
+    $this->tideSiteHelper = $this->createMock(TideSiteHelper::class);
+    $this->aliasHelper = new AliasStorageHelper($this->tideSiteHelper, $this->entityTypeManager);
   }
 
   function providerAliasWithoutSite() {
@@ -81,6 +69,54 @@ class TideSiteAliasTest extends UnitTestCase {
       [['alias' => '/hello_5'], '/hello_5'],
       [['alias' => '/node/1'], '/node/1'],
     ];
+  }
+
+  /**
+   * @dataProvider providerAliasWithoutSite
+   */
+  function testgetPathAliasWithoutSitePrefixNew($alias_with_site, $expection) {
+    $without_siteid = $this->aliasHelper->getPathAliasWithoutSitePrefix($alias_with_site);
+    $this->assertEquals($expection, $without_siteid);
+  }
+
+  function providerIsPathHasSitePrefix() {
+    return [
+      ['/site-10/hello-1', TRUE],
+      ['/site/hello-1', FALSE],
+      ['/10/hello-1', FALSE],
+      ['/site-1/site-hello-1', TRUE],
+      ['/site-10/site-2/-a1', TRUE],
+    ];
+  }
+
+  /**
+   * @dataProvider providerIsPathHasSitePrefix
+   */
+  function testIsPathHasSitePrefixNew($alias, $expectation) {
+    $methods = get_class_methods('Drupal\path_alias\Entity\PathAlias');
+    $path_alias = $this->getMockBuilder('Drupal\path_alias\Entity\PathAlias')
+      ->disableOriginalConstructor()
+      ->setMethods($methods)
+      ->getMock();
+    $path_alias->expects($this->any())->method('getAlias')->willReturn($alias);
+    $this->assertEquals($expectation, $this->aliasHelper->isPathHasSitePrefix($path_alias));
+  }
+
+  function testIsAliasExists() {
+    $methods = get_class_methods('Drupal\path_alias\Entity\PathAlias');
+    $path_alias = $this->getMockBuilder('Drupal\path_alias\Entity\PathAlias')
+      ->disableOriginalConstructor()
+      ->setMethods($methods)
+      ->getMock();
+    $path_alias->expects($this->any())
+      ->method('getAlias')
+      ->willReturn('/site-1/hello');
+    $this->entityStorage->expects($this->any())
+      ->method('loadByProperties')
+      ->willReturn([$path_alias]);
+    $this->assertEquals('/site-1/hello', $this->aliasHelper->isAliasExists('/site-1/hello')
+      ->getAlias());
+    $this->assertEquals($path_alias, $this->aliasHelper->isAliasExists('/site-1/hello'));
   }
 
 }
