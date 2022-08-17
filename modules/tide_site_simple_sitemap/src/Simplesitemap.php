3c6ee9a34a84252a779af051071e774fb0f180ff
Vincent Gao <vincent.gao@dpc.vic.gov.au>
2022-08-17 10:57:31 +1000

    Update fetchSitemapChunk

--- modules/tide_site_simple_sitemap/src/Simplesitemap.php
+++ modules/tide_site_simple_sitemap/src/Simplesitemap.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\tide_site_simple_sitemap;
 
+use Drupal\simple_sitemap\Plugin\simple_sitemap\SitemapGenerator\SitemapGeneratorBase;
 use Drupal\simple_sitemap\Simplesitemap as DefaultSimplesitemap;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -36,35 +37,50 @@ class Simplesitemap extends DefaultSimplesitemap {
   protected function fetchSitemapChunk($id) {
     $site_id = $this->request->get('site');
     $page = $this->request->get('page');
-    try {
-      if (!empty($site_id) && !empty($page)) {
-        return $this->db->query('SELECT * FROM {simple_sitemap_site} WHERE delta = :delta and site_id = :site_id',
-          [':delta' => $page, ':site_id' => $site_id])->fetchObject();
-      }
-      elseif (empty($site_id)) {
-        return parent::fetchSitemapChunk($id);
-      }
-      else {
-        if ($this->hasSiteMapChunks()) {
-          return $this->db->query('SELECT * FROM {simple_sitemap_site} WHERE delta = :delta and site_id = :site_id',
-            [':delta' => 0, ':site_id' => $site_id])->fetchObject();
-        }
-        return $this->db->query('SELECT * FROM {simple_sitemap_site} WHERE site_id = :site_id',
-          [':site_id' => $site_id])->fetchObject();
-      }
-    } catch (\Exception $exception) {
-      throw new NotFoundHttpException();
+    // If no site_id provided, returns its parent.
+    if (empty($site_id)) {
+      return parent::fetchSitemapChunk($id);
     }
+    // If the page number is provided, return the chunk.
+    if (!empty($page)) {
+      $result = $this->db->query('SELECT * FROM {simple_sitemap_site} WHERE delta = :delta and site_id = :site_id',
+        [':delta' => $page, ':site_id' => $site_id])->fetchObject();
+      return $this->validateResult($result);
+    }
+    // Returns the site-based pagination xml if it exists.
+    if ($this->hasSiteMapChunks()) {
+      $result = $this->db->query('SELECT * FROM {simple_sitemap_site} WHERE delta = :delta and site_id = :site_id',
+        [':delta' => SitemapGeneratorBase::INDEX_DELTA, ':site_id' => $site_id])
+        ->fetchObject();
+      return $this->validateResult($result);
+    }
+    // Returns the site-based xml if it exists.
+    $result = $this->db->query('SELECT * FROM {simple_sitemap_site} WHERE site_id = :site_id',
+      [':site_id' => $site_id])->fetchObject();
+    return $this->validateResult($result);
   }
 
+  /**
+   * Checks if tide_simple_site_map has chunks.
+   */
   protected function hasSiteMapChunks() {
-    $result = \Drupal::database()->select('simple_sitemap_site', 's')
+    $result = $this->db->select('simple_sitemap_site', 's')
       ->fields('s', ['delta'])
       ->condition('s.status', 1)
-      ->condition('s.delta', '0', '=')
+      ->condition('s.delta', SitemapGeneratorBase::INDEX_DELTA)
       ->execute()
       ->fetchAll();
     return !empty($result);
+  }
+
+  /**
+   * Checks if the result is valid.
+   */
+  protected function validateResult($result) {
+    if (empty($result)) {
+      throw new NotFoundHttpException();
+    }
+    return $result;
   }
 
 }
